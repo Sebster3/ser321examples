14ca82c425c1f73b44508d6bee272e2d0c671f17
Sebastien De La Cruz <sadelacr@asu.edu>
2020-10-28 01:22:23 +0000

    Work on 'github' query

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -233,10 +233,15 @@ class WebServer {
 
           Map<String, String> query_pairs = new LinkedHashMap<String, String>();
           query_pairs = splitQuery(request.replace("github?", ""));
+	  // String url = "https://api.github.com/" + query_pairs.get("query");
+	  // System.out.println(url);
           String json = fetchURL("https://api.github.com/" + query_pairs.get("query"));
-          System.out.println(json);
+          // System.out.println(json);
 
-          builder.append("Check the todos mentioned in the Java source file");
+	  builder.append("HTTP/1.1 200 OK\n");
+	  builder.append("Content-Type: text/html; charset=utf-8\n");
+	  builder.append("\n");
+          builder.append(json);
           // TODO: Parse the JSON returned by your fetch and create an appropriate
           // response
           // and list the owner name, owner id and name of the public repo on your webpage, e.g.
